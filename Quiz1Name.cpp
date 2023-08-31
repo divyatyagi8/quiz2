32d5b1fe3096f4d1d3002c02a169d2f6fa3fac2b
divyatyagi8 <134290834+divyatyagi8@users.noreply.github.com>
2023-08-31 17:35:50 -0400

    Add files via upload

---
+++ Quiz1Name.cpp
@@ -0,0 +1,12 @@
+﻿// Quiz1Name.cpp : Defines the entry point for the application.
+//
+
+#include "Quiz1Name.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Divya Tyagi" << endl;
+	return 0;
+}
